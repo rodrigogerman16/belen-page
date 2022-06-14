c676b0f27987f7aa7a931b321a45f1a8e8f85cd9
Rodrigo German <rodrigoezequielgerman@gmail.com>
2022-06-14 11:57:08 +0200

    Component to export the gallery png

---
+++ src/Components/images.js
@@ -0,0 +1,9 @@
+import image1 from '../Assets/gallery1.png'
+import image2 from '../Assets/gallery2.png'
+import image3 from '../Assets/gallery3.png'
+import image4 from '../Assets/gallery4.png'
+import image5 from '../Assets/gallery5.png'
+import image6 from '../Assets/gallery6.png'
+import image7 from '../Assets/gallery7.png'
+
+export default [image1, image2, image3, image4, image5, image6, image7]
