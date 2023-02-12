ec8e2bd66d994271fbef7ca6fd72d1b3115db161
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-12 13:47:29 +0100

    New responsive reviews

--- src/Components/ImageSlider.js
+++ src/Components/ImageSlider.js
@@ -1,36 +1,134 @@
-import React, {useState} from 'react'
-import { SliderData } from './SliderData'
-import { FaArrowAltCircleLeft, FaArrowAltCircleRight } from 'react-icons/fa'
+import React from "react";
 
-const ImageSlider = ({slides}) => {
-
-  const [current, setCurrent] = useState(0);
-  const length = slides.length;
-
-  const nextSlide = () => {
-      setCurrent(current === length - 1 ? 0 : current + 1)
-  }
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1)
-  }
-
-  if(!Array.isArray(slides)|| slides.length <= 0) {return null;}
+const ImageSlider = () => {
 
   return (
-    <section className='Slider'>      
-        <FaArrowAltCircleLeft className='Left-arrow' onClick={prevSlide}/>
-        <FaArrowAltCircleRight className='Right-arrow' onClick={nextSlide}/>
-        {SliderData.map((slide, index) =>{
-            return (
-                  <div className={index === current ? 'slide active' : 'slide'} key={index}>
-                    {prevSlide}
-                    {index === current && <img src={slide.image} alt="img" className='Image'/>}   
-                    {nextSlide}              
-                  </div>
-              )       
-        })}
-    </section>
-  )
-}
+    <div>
+      <section className="my-14 text-gray-700 w-3/4 m-auto">
+        <div className="text-center mb-20 md:max-w-xl lg:max-w-3xl mx-auto">
+          <h3 className="text-3xl font-bold mb-6 text-gray-800">Testimonials</h3>
+          <p className="mb-6 text-justify pb-2 md:mb-12 md:pb-0">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+            error amet numquam iure provident voluptate esse quasi, veritatis
+            totam voluptas nostrum quisquam eum porro a pariatur veniam.
+          </p>
+        </div>
 
-export default ImageSlider
+        <div className="grid md:grid-cols-2 gap-6 lg:gap-12 text-center">
+        <div className="mb-0">
+            <div className="flex justify-center mb-6">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
+                className="rounded-full shadow-lg w-32"
+              />
+            </div>
+            <h5 className="text-xl font-semibold mb-4">Klaudia</h5>
+            <p className="mb-4">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="quote-left"
+                className="w-6 pr-2 inline-block"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+                ></path>
+              </svg>
+              I have been taking Belen's flexibility classes for more than two years and being part of her beautiful community of girls from all over the world. Not only my flexibility impove a lot when dancing but also the pain in my back and trapezius muscles that i used to have all my life are gone completely. Each class is different and challenges us to something new, which i love.
+            </p>
+          </div>
+
+          <div className="mb-12 md:mb-0">
+            <div className="flex justify-center mb-6">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(1).jpg"
+                className="rounded-full shadow-lg w-32"
+              />
+            </div>
+            <h5 className="text-xl font-semibold mb-4">Anastasiya</h5>
+            <p className="mb-4">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="quote-left"
+                className="w-6 pr-2 inline-block"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+                ></path>
+              </svg>
+              Belen is an amazing teacher. Her natural flexibility combined with very technical and knowledgeable approach motivates me to work out constantly and continuously. Her flexibility classes are a great exercise for everyone independent of the level.
+            </p>
+          </div>
+          <div className="mb-12 md:mb-0">
+            <div className="flex justify-center mb-6">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(2).jpg"
+                className="rounded-full shadow-lg w-32"
+              />
+            </div>
+            <h5 className="text-xl font-semibold mb-4">Jenny</h5>
+            <p className="mb-4">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="quote-left"
+                className="w-6 pr-2 inline-block"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+                ></path>
+              </svg>
+              I have been doing the flexibility classes for over a year now and i have become so much more flexible. Every class is both challenging and relaxing. For me, Belen's flexibility classes are like 1 hour meditations, i can truly focus on my body and the movements.
+            </p>
+          </div>
+          <div className="mb-0">
+            <div className="flex justify-center mb-6">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Avatars/img%20(9).jpg"
+                className="rounded-full shadow-lg w-32"
+              />
+            </div>
+            <h5 className="text-xl font-semibold mb-4">Ewa</h5>
+            <p className="mb-4">
+              <svg
+                aria-hidden="true"
+                focusable="false"
+                data-prefix="fas"
+                data-icon="quote-left"
+                className="w-6 pr-2 inline-block"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 512 512"
+              >
+                <path
+                  fill="currentColor"
+                  d="M464 256h-80v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8c-88.4 0-160 71.6-160 160v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48zm-288 0H96v-64c0-35.3 28.7-64 64-64h8c13.3 0 24-10.7 24-24V56c0-13.3-10.7-24-24-24h-8C71.6 32 0 103.6 0 192v240c0 26.5 21.5 48 48 48h128c26.5 0 48-21.5 48-48V304c0-26.5-21.5-48-48-48z"
+                ></path>
+              </svg>
+              I absolutely love Belen's flexibility classes! The exercises are not only helping me to become more flexible but also have a positive effect on my dancing. Belen's wide variety of exercises & professional feedback are very useful. Thank you so much for inspiring us and for the amazing training!
+            </p>
+          </div>
+          
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default ImageSlider;
