c71acf4b63fafdc133b794410836253ac561f509
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-15 13:25:09 +0100

    minimal css change

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -38,7 +38,7 @@ const Form = () => {
         onSubmit={sendData}
       >
         <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
-          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+          <h2 class="mb-4 text-2xl tracking-tight font-bold text-center text-gray-900">
             Contact Me
           </h2>
           <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
