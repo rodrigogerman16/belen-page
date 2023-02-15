861be7828347e20571d2e58180d250044b93a25d
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-15 13:19:56 +0100

    new form

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,51 +1,109 @@
-import React, {Fragment, useState}  from 'react';
-import emailjs from 'emailjs-com';
+import React, { Fragment, useState } from "react";
+import emailjs from "emailjs-com";
 
 const Form = () => {
+  const [datos, setDatos] = useState({
+    email: "",
+    subject: "",
+    message: "",
+  });
 
-    const [datos, setDatos] = useState ({
-        name: '',
-        phone: '',
-        email: '',
-        message: '',
-    })
+  const handleInputChange = (e) => {
+    setDatos({
+      ...datos,
+      [e.target.name]: e.target.value,
+    });
+  };
 
-    const handleInputChange = (e) => {
-        setDatos({
-            ...datos,
-        [e.target.name] : e.target.value
-        })        
-    }
+  const sendData = (e) => {
+    e.preventDefault();
 
-    const sendData = (e) => {
-        e.preventDefault();
+    emailjs
+      .sendForm(
+        "service_05y8hs1",
+        "template_96u0nmf",
+        e.target,
+        "3JrmPJmxGZpTsDh1a"
+      )
+      .then((res) => {
+        alert("Your message has been sent.");
+      });
+  };
 
-        emailjs.sendForm('service_05y8hs1', 'template_96u0nmf', e.target, '3JrmPJmxGZpTsDh1a').then (res=>{
-            alert('Your message has been sent.');
-    })
-    }
-
-    return (
-        <Fragment>
-            <div className='Contact-container' id='Contact' onSubmit={sendData}>
-            <div className='Message-form'>
-            <h1>Do you need dance lessons, a face to your brand or workshop in your country?</h1>
-            <h2>¡Contact me!</h2>
-            <div className='Border'></div>  
-            <form className='Contact-form' action="index.html" method="post">
-                <input type="text" className='Contact-form-text' placeholder='Name' name='name' onChange={handleInputChange}/>
-                <input type="text" className='Contact-form-text' placeholder='Phone' name='phone'onChange={handleInputChange}/>
-                <input type="email" className='Contact-form-text' placeholder='Email' name='email'onChange={handleInputChange}/>
-                <textarea type="text" className='Contact-form-text' placeholder='Message' name='message'onChange={handleInputChange}/>
-                <input type="submit" className='Contact-form-btn' value='Send'/>
-            </form>
+  return (
+    <Fragment>
+      <section
+        class="bg-white dark:bg-gray-900"
+        id="Contact"
+        onSubmit={sendData}
+      >
+        <div class="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
+          <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
+            Contact Me
+          </h2>
+          <p class="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
+            Do you need dance lessons, a face to your brand or workshop in your
+            country?
+          </p>
+          <form action="index.html" method="post" class="space-y-8">
+            <div>
+              <label
+                for="email"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                name="email"
+                class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="example@email.com"
+                onChange={handleInputChange}
+                required
+              />
             </div>
-            <div className='Sponsors'>
-            LOGO LOGO LOGO
-            </div>        
+            <div>
+              <label
+                for="subject"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Subject
+              </label>
+              <input
+                type="text"
+                name="subject"
+                class="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+                placeholder="Let us know how we can help you"
+                required
+                onChange={handleInputChange}
+              />
+            </div>
+            <div class="sm:col-span-2">
+              <label
+                for="message"
+                class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+              >
+                Your message
+              </label>
+              <textarea
+                name="message"
+                rows="6"
+                class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Leave a comment..."
+                onChange={handleInputChange}
+              ></textarea>
+            </div>
+            <button
+              type="submit"
+              class="py-3 px-5 text-sm font-medium text-center rounded-lg bg-gray-100 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300"
+            >
+              Send message
+            </button>
+          </form>
         </div>
-      </Fragment>
-    );
-}
+      </section>
+    </Fragment>
+  );
+};
 
 export default Form;
