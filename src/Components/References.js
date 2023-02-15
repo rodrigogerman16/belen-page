dd08717df51bed2db9f35f4901ec250630794e4f
Rodrigo German <rodrigoezequielgerman@gmail.com>
2023-02-15 13:31:55 +0100

    testimonial description

--- src/Components/References.js
+++ src/Components/References.js
@@ -8,10 +8,8 @@ const References = () => {
           <h3 className="text-3xl font-bold mb-6 text-gray-800">
             Testimonials
           </h3>
-          <p className="mb-6 text-justify pb-2 md:mb-12 md:pb-0">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
-            error amet numquam iure provident voluptate esse quasi, veritatis
-            totam voluptas nostrum quisquam eum porro a pariatur veniam.
+          <p className="mb-6 text-center pb-2 md:mb-12 md:pb-0">
+          As a dance and flexibility teacher, i touched the lives of many students, inspiring them to embrace their love of dance and grow in their skills and confidence. Below are just a few of the experiences shared by them:
           </p>
         </div>
 
